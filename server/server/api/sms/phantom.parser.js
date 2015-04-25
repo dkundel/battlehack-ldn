90199d95a20f92d902083009ad7b03c81f415367
npentrel <npentrel@gmail.com>
2015-04-25 21:29:50 +0100

    better

--- server/server/api/sms/phantom.parser.js
+++ server/server/api/sms/phantom.parser.js
@@ -17,41 +17,40 @@ exports.parse = function(text, query, callback) {
 	phantom.create(function (ph) {
 	  ph.createPage(function (page) {
 	    page.open(query.url, function (status) {
-	      console.log("hello ", status);
 	      console.log(query.selector);
 	      page.evaluate(function (query) {
-	      	return document.querySelector(query.selector).innerText;
-	      	// jquery $('#mw-content-text p:nth-of-type(1)').text();
+	      	console.log("INSIDE" + query.selector);
+		  	return $(query.selector).text()
 	      }, function (result) {
-	        console.log('RESULT:\n ' + JSON.stringify(result));
+	        console.log('RESULT1:\n ' + JSON.stringify(result));
 	        // FIND FIRST WIKIPEDIA RESULT
-	        if (result === "Other reasons this message may be displayed:") {
+	        if (result.indexOf("does not exist") > -1) {
 	        	console.log("query wrong");
-	        	var search_url = "http://en.wikipedia.org/w/index.php?search=" + text.replace(' ', '+');
+	        	var search_url = query.selector + text.replace(' ', '+');
  	        	page.open(search_url, function(status) {
 	        		console.log("NO");
 	        		page.evaluate(function () {
 	        			return document.querySelector('.mw-search-result-heading>a').href;
 	        		}, function (result) {
 	        			console.log("SEARCH URL: " + search_url);
-	        			console.log("RESULT: " + JSON.stringify(result));
+	        			console.log("RESULT2: " + JSON.stringify(result));
 	        			page.open(result, function(status) {
 	        				console.log("FIXED");
 	        				page.evaluate(function () {
 						      	return document.querySelector('#mw-content-text p:nth-of-type(1)').innerText;
-	        				}, function (result) {
-	        					console.log('RESULT:\n ' + JSON.stringify(result));
+	        				}, function (next_result) {
+	        					console.log('RESULT:\n ' + JSON.stringify(next_result));
 						        ph.exit();
-						        callback(result);
-	        				});
+ 						        callback(next_result);
+	        				}, query);
 	        			});
-	        		});
+	        		}, query);
 	        	});
 	        } else {
 		        ph.exit();
 		        callback(result);
 	        }
-	      });
+	      }, query);
 	    });
 	  });
 	});
